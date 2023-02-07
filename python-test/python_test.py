6e8e9b0e1a2e145ad875e9a3b5db2d1a6d9c39c0
Dmitrii Kalashnikov <statsuperusa@gmail.com>
2023-02-07 21:45:56 +0400

    try simple script

--- python-test/python_test.py
+++ python-test/python_test.py
@@ -1,26 +1,26 @@
-# import requests
-
-# # URL of the microservice
-# url = "http://0.0.0.0:5000"
-
-# # Send a GET request to the microservice
-# response = requests.get(url)
-
-# # Print the status code of the response
-# print(f"Status code: {response.status_code}")
-
-# # Print the content of the response
-# print(f"Response content: {response.content}")
-
 import requests
-from requests.adapters import HTTPAdapter
-from urllib3.util.retry import Retry
 
-session = requests.Session()
-retry = Retry(total=5, backoff_factor=0.1, status_forcelist=[ 500, 502, 503, 504 ])
-adapter = HTTPAdapter(max_retries=retry)
-session.mount('http://', adapter)
+# URL of the microservice
+url = "http://172.17.0.2:5000"
 
-response = session.get("http://172.17.0.2:5000")
+# Send a GET request to the microservice
+response = requests.get(url)
 
-print(response.text)
+# Print the status code of the response
+print(f"Status code: {response.status_code}")
+
+# Print the content of the response
+print(f"Response content: {response.content}")
+
+# import requests
+# from requests.adapters import HTTPAdapter
+# from urllib3.util.retry import Retry
+
+# session = requests.Session()
+# retry = Retry(total=5, backoff_factor=0.1, status_forcelist=[ 500, 502, 503, 504 ])
+# adapter = HTTPAdapter(max_retries=retry)
+# session.mount('http://', adapter)
+
+# response = session.get("http://172.17.0.2:5000")
+
+# print(response.text)
