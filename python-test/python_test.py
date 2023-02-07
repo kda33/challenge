8cd682e8902b96dc160b1bdf078a487f93574309
Dmitrii Kalashnikov <statsuperusa@gmail.com>
2023-02-07 17:29:48 +0400

    add localhost ip

--- python-test/python_test.py
+++ python-test/python_test.py
@@ -21,6 +21,6 @@ retry = Retry(total=5, backoff_factor=0.1, status_forcelist=[ 500, 502, 503, 504
 adapter = HTTPAdapter(max_retries=retry)
 session.mount('http://', adapter)
 
-response = session.get("http://0.0.0.0:5000")
+response = session.get("http://172.17.0.2:5000")
 
 print(response.text)
